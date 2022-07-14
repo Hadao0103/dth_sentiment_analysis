a159d154daf2ce88c267be36514c85d5071c61ae
Hadao0103 <99713402+Hadao0103@users.noreply.github.com>
2022-07-14 13:08:13 +0700

    Update project2_sentiment_v2.py

--- project2_sentiment_v2.py
+++ project2_sentiment_v2.py
@@ -17,13 +17,7 @@ import regex
 import demoji
 import emoji
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.model_selection import train_test_split  
-
-import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-
+from sklearn.model_selection import train_test_split 
 
 # model
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
